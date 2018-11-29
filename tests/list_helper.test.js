c4c546e36465a8a7343b1c3694bf7c022578dc98
Olli Eskola <olli.eskola@siili.com>
2018-11-29 15:54:55 +0200

    tehtävä 4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,3 +6,90 @@ test('dummy is called', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ]
+
+    const listWithTwoBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 15,
+          __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 0,
+            __v: 0
+          }
+      ]
+
+      const listWithFourBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 100,
+          __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 100,
+            __v: 0
+          },
+          {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 100,
+            __v: 0
+          },
+          {
+              _id: '5a422aa71b54a676234d17f8',
+              title: 'Go To Statement Considered Harmful',
+              author: 'Edsger W. Dijkstra',
+              url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+              likes: 100,
+              __v: 0
+            }
+      ]
+  
+    test('when list has only one blog equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      expect(result).toBe(5)
+    })
+
+    test('when list has two blogs equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithTwoBlogs)
+        expect(result).toBe(15)
+    })
+
+    test('when list has four blogs equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithFourBlogs)
+        expect(result).toBe(400)
+    })
+
+    test('when list has no blogs equals 0', () => {
+        const result = listHelper.totalLikes([])
+        expect(result).toBe(0)
+    })
+  })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+    let sum = 0
+    blogs.forEach(e => {
+        sum += e.likes
+    })
+    return sum
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
